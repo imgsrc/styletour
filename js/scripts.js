244ad41e52e8129acf43e57eb8ad58b944aa835d
imgsrc <nikolaev.i@outlook.com>
2016-07-13 20:11:25 +0400

    search

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,7 @@ $(function(){
     $('.carousel').carousel({
         interval: false
     })
+    $('.search').on('click', function () {
+       $('.main-menu .navbar-form').slideToggle();
+    });
 });
